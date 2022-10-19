a4992d84198ced243114b888ea0ae8164e94c8d9
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-10-20 02:47:32 +0700

    dc: Add Scanner of the java.util package to read input data from user

---
+++ introduction-to-java-api/FindTriangularNumber/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Введите целое неотрицательное число: ");
+
+		int n = scan.nextInt();
+		scan.close();
+	}
+}
