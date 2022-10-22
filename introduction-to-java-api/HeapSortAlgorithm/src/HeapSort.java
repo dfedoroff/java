f9bccb0b4b5ca94d2379c34fd152dda65facb872
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-10-22 11:17:07 +0700

    refactor: Rename variables

--- introduction-to-java-api/HeapSortAlgorithm/src/HeapSort.java
+++ introduction-to-java-api/HeapSortAlgorithm/src/HeapSort.java
@@ -1,10 +1,10 @@
 public class HeapSort {
 
-	private static void swap(int a, int b) {
+	private static void swap(int[] arr, int a, int b) {
 
-		int temp = a;
-		a = b;
-		b = temp;
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
 	}
 
 	public static void sort(int[] arr) {
@@ -12,16 +12,16 @@ public class HeapSort {
 		int len = arr.length;
 
 		for (int i = len / 2 - 1; i >= 0; i--) {
-			buildHeap(arr, i, len);
+			buildHeap(arr, len, i);
 		}
 
 		for (int i = len - 1; i >= 0; i--) {
-			swap(arr[0], arr[i]);
-			buildHeap(arr, 0, i);
+			swap(arr, 0, i);
+			buildHeap(arr, i, 0);
 		}
 	}
 
-	public static void buildHeap(int[] arr, int i, int heapLen) {
+	public static void buildHeap(int[] arr, int heapLen, int i) {
 
 		int left = 2 * i + 1;
 		int right = 2 * i + 2;
@@ -36,7 +36,7 @@ public class HeapSort {
 		}
 
 		if (max != i) {
-			swap(arr[i], arr[max]);
+			swap(arr, i, max);
 			buildHeap(arr, heapLen, max);
 		}
 	}
