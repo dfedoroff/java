63d16401068cc4ce2e8a8c2e71854652b6c287a0
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-10-25 10:44:11 +0700

    feat: Scan maze by checking surrounding cells according to specified conditions and assign step + 1 into such cells

--- introduction-to-java-api/WaveAlgorithm/src/WaveAlgo.java
+++ introduction-to-java-api/WaveAlgorithm/src/WaveAlgo.java
@@ -1,6 +1,38 @@
 public class WaveAlgo {
 
     /**
+     * Принимаем step в качестве аргумента. Сканируем массив циклом for и если
+     * находим число равное кол-ву step проверяем окружающие ячейки:
+     * - содержат ли они числа
+     * - являются ли они границами лабиринта
+     * Записываем step + 1 в такие ячейки
+     */
+    private static void makeStep(int[][] maze, int[][] mazeTmp, int step) {
+
+        int lenRow = mazeTmp.length - 1;
+        int lenCol = mazeTmp[0].length - 1;
+
+        for (int i = 1; i < lenRow; i++) {
+            for (int j = 1; j < lenCol; j++) {
+                if (mazeTmp[i][j] == step) {
+                    if (mazeTmp[i - 1][j] == 0 && maze[i - 1][j] == 0) {
+                        mazeTmp[i - 1][j] = step + 1;
+                    }
+                    if (mazeTmp[i][j - 1] == 0 && maze[i][j - 1] == 0) {
+                        mazeTmp[i][j - 1] = step + 1;
+                    }
+                    if (mazeTmp[i + 1][j] == 0 && maze[i + 1][j] == 0) {
+                        mazeTmp[i + 1][j] = step + 1;
+                    }
+                    if (mazeTmp[i][j + 1] == 0 && maze[i][j + 1] == 0) {
+                        mazeTmp[i][j + 1] = step + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
      * Считаем количество шагов
      */
     public static void countSteps(int[][] maze, int[][] mazeTmp) {
