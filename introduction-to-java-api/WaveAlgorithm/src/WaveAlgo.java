268d8bd05413dbcc86943d233b0513b481ce5acf
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-10-25 10:57:41 +0700

    feat: Print maze

--- introduction-to-java-api/WaveAlgorithm/src/WaveAlgo.java
+++ introduction-to-java-api/WaveAlgorithm/src/WaveAlgo.java
@@ -94,4 +94,19 @@ public class WaveAlgo {
             System.out.println(split[i] + " ");
         }
     }
+
+    public static void printArray(int[][] arr) {
+
+        int lenRow = arr.length;
+        int lenCol = arr[0].length;
+
+        for (int i = 0; i < lenRow; i++) {
+            for(int j = 0; j < lenCol; j++) {
+                System.out.print(arr[i][j] + " ");
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
