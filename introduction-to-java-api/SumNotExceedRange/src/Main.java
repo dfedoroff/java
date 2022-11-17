a10bfff2eeeebdb8d8ec369763ef44f392ea6795
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-17 16:02:09 +0700

    dc: Initialize variables

---
+++ introduction-to-java-api/SumNotExceedRange/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		int n = validateIntInput();
+		int m = validateIntInput();
+	}
+
+	public static int validateIntInput() {
+		Scanner scan = new Scanner(System.in);
+
+		int num = 0;
+		boolean isValid = false;
+
+		while (!isValid) {
+			System.out.print("Введите целое число: ");
+			String input = scan.nextLine();
+
+			if (input == null || input.equals("")) {
+				throw new IllegalArgumentException("Пустая строка.");
+			}
+
+			try {
+				num = Integer.parseInt(input);
+				isValid = true;
+			} catch (NumberFormatException e) {
+				System.out.println("Это не целое число.");
+			}
+		}
+		return num;
+	}
+}
