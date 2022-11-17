7ed1ea127ace539bc8c2269bbed81aac77562124
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-17 12:21:22 +0700

    refactor: Move integer input validation method from IntegerInput class to Main class

--- introduction-to-java-api/CheckNumberSign/src/Main.java
+++ introduction-to-java-api/CheckNumberSign/src/Main.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 
-		int num = IntegerInput.check();
+		int num = validateIntInput();
 
 		System.out.print(SignCheck.isNegative(num));
+	}
+
+	public static int validateIntInput() {
+		Scanner scan = new Scanner(System.in);
+
+		int n = 0;
+		boolean isValid = false;
+
+		while (!isValid) {
+			System.out.print("Введите целое число: ");
+			String input = scan.nextLine();
+
+			if (input == null || input.equals("")) {
+				throw new IllegalArgumentException("Пустая строка.");
+			}
+
+			try {
+				n = Integer.parseInt(input);
+				isValid = true;
+			} catch (NumberFormatException e) {
+				System.out.println("Это не целое число.");
+			}
+		}
+		scan.close();
+		return n;
 	}
 }
