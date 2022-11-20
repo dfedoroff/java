be59eb914f3a35331b12957d8ac57accddfa0093
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-20 10:05:20 +0700

    feat: Multiply elements of array of integers that are less than 6

--- introduction-to-java-api/MultiplyNumsLess6/src/ArrayManip.java
+++ introduction-to-java-api/MultiplyNumsLess6/src/ArrayManip.java
@@ -1,6 +1,17 @@
 import java.util.Arrays;
 
 public class ArrayManip {
+	static void multiply(int[] arr) {
+
+		int len = arr.length;
+
+		for (int i = 0; i < len; i++) {
+			if (arr[i] < 6) {
+				arr[i] *= 2;
+			}
+		}
+	}
+
 	public static void printArray(int[] arr) {
 		System.out.println(Arrays.toString(arr));
 	}
