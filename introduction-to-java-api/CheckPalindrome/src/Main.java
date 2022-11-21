5bfd2a0cbb6677020eed2e0040eb0eab7d29ea1e
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-21 10:40:44 +0700

    dc: Initialize variable

---
+++ introduction-to-java-api/CheckPalindrome/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		String myStr = validateStrInput();
+	}
+
+	public static String validateStrInput() {
+		Scanner scan = new Scanner(System.in);
+
+		String str = "";
+		boolean isValid = false;
+
+		while (!isValid) {
+			System.out.print("Введите палиндром: ");
+			String input = scan.nextLine();
+
+			if ((input != null) && (!input.equals(""))) {
+				str = input;
+				isValid = true;
+			} else {
+				System.out.println("Вы ничего не ввели.");
+			}
+		}
+		return str;
+	}
+}
