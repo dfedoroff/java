35a0c4f01a17ddc277664e77ae32b9aa1a22c1cc
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-21 10:58:09 +0700

    dc: Print True or False depending on user-defined string is palindrome or not

--- introduction-to-java-api/CheckPalindrome/src/Main.java
+++ introduction-to-java-api/CheckPalindrome/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		String myStr = validateStrInput();
+		System.out.print(PalindromeValid.isPalindrome(myStr));
 	}
 
 	public static String validateStrInput() {
