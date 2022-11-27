7d0c5b4a9665ac57ab0dddb4051ce7e95d475bd3
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-27 11:10:17 +0700

    dc: Add predefined contacts to phonebook using add methodfrom PhoneBook class

---
+++ introduction-to-java-api/PhoneBook/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+
+		PhoneBook phoneBook = new PhoneBook();
+
+		phoneBook.add("Gibson", "33344");
+		phoneBook.add("Hamilton", "344");
+		phoneBook.add("Brooks", "344");
+		phoneBook.add("Brooks", "122");
+	}
+}
