9dfdc3f4e1f7b1b991614e1add30629863c44858
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-29 00:34:54 +0700

    refactor: Reinitialize charactersLimit variable and remove old variables

--- java-oop/HomeWork2/src/Main.java
+++ java-oop/HomeWork2/src/Main.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 public class Main {
 	public static void main(String[] args) {
 
-		int charactersLimit = 50;
-		int charactersUnique = characters.size();
-		String characterSelected = "Monk";
+		int charactersLimit = 10;
 	}
 }
