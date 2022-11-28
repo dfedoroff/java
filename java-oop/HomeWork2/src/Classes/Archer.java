eb57c382fcdab1f4bdba7e259c75f6568c573416
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-29 00:10:25 +0700

    refactor: Implement interface Stepable in Archer class

--- java-oop/HomeWork2/src/Classes/Archer.java
+++ java-oop/HomeWork2/src/Classes/Archer.java
@@ -1,6 +1,17 @@
-public class Archer extends BasicCharacter {
+package Classes;
 
-    public Archer() {
-        super(6, 3, 16, new int[]{2,3}, 10, 4, false, false);
+import Interfaces.Stepable;
+
+import java.util.ArrayList;
+
+public class Archer extends BasicCharacter implements Stepable {
+
+    public Archer(ArrayList<BasicCharacter> squad) {
+        super(6, 3, 16, new int[]{2, 3}, 10, 4, false, false);
+        super.squad = squad;
+    }
+
+    @Override
+    public void step() {
     }
 }
