ad4dc69ec55759c0c012b0e15e3df537cbfdd0d7
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-29 00:19:30 +0700

    refactor: Implement interface Stepable in Spearman class

--- java-oop/HomeWork2/src/Classes/Spearman.java
+++ java-oop/HomeWork2/src/Classes/Spearman.java
@@ -1,6 +1,17 @@
-public class Spearman extends BasicCharacter {
+package Classes;
 
-    public Spearman() {
+import Interfaces.Stepable;
+
+import java.util.ArrayList;
+
+public class Spearman extends BasicCharacter implements Stepable {
+
+    public Spearman(ArrayList<BasicCharacter> squad) {
         super(4, 5, 0, new int[]{1,3}, 10, 4, false, false);
+        super.squad = squad;
+    }
+
+    @Override
+    public void step() {
     }
 }
