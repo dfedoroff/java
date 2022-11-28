682f6df33e0c11865b542ff53a773aecbdcdd2c9
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-29 00:38:56 +0700

    dc: Initialize variables for squads

--- java-oop/HomeWork2/src/Main.java
+++ java-oop/HomeWork2/src/Main.java
@@ -1,9 +1,13 @@
+import Classes.*;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
 
 		int charactersLimit = 10;
+		int charactersBrightSide = 5;
+		int charactersDarkSide = 4;
 	}
 }
