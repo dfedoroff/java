840d95446a0fde12c622f9988cf32e4fa88e817d
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-29 00:16:44 +0700

    refactor: Implement interface Stepable in Outlaw class

--- java-oop/HomeWork2/src/Classes/Outlaw.java
+++ java-oop/HomeWork2/src/Classes/Outlaw.java
@@ -1,6 +1,17 @@
-public class Outlaw extends BasicCharacter {
+package Classes;
 
-    public Outlaw() {
+import Interfaces.Stepable;
+
+import java.util.ArrayList;
+
+public class Outlaw extends BasicCharacter implements Stepable {
+
+    public Outlaw(ArrayList<BasicCharacter> squad) {
         super(8, 3, 0, new int[]{2,4}, 10, 6, false, false);
+        super.squad = squad;
+    }
+
+    @Override
+    public void step() {
     }
 }
