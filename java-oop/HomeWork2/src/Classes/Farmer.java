37f4c36a9a61e916acce07357561f58f8f503f7e
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-29 00:13:17 +0700

    refactor: Implement interface Stepable in Farmer class

--- java-oop/HomeWork2/src/Classes/Farmer.java
+++ java-oop/HomeWork2/src/Classes/Farmer.java
@@ -1,6 +1,17 @@
-public class Farmer extends BasicCharacter {
+package Classes;
 
-    public Farmer() {
+import Interfaces.Stepable;
+
+import java.util.ArrayList;
+
+public class Farmer extends BasicCharacter implements Stepable {
+
+    public Farmer(ArrayList<BasicCharacter> squad) {
         super(1, 1, 0, new int[]{1,1}, 1, 3, true, false);
+        super.squad = squad;
+    }
+
+    @Override
+    public void step() {
     }
 }
