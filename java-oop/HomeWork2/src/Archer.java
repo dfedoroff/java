4c30f003926b08ee5d1f6c77715fdb6dc86bb1df
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-28 11:25:47 +0700

    refactor: Remove all moved to Classes folder game characters classes from src folder

--- java-oop/HomeWork2/src/Archer.java
+++
@@ -1,6 +0,0 @@
-public class Archer extends BasicCharacter {
-
-    public Archer() {
-        super(6, 3, 16, new int[]{2,3}, 10, 4, false, false);
-    }
-}
--- java-oop/HomeWork2/src/Outlaw.java
+++
@@ -1,6 +0,0 @@
-public class Outlaw extends BasicCharacter {
-
-    public Outlaw() {
-        super(8, 3, 0, new int[]{2,4}, 10, 6, false, false);
-    }
-}
