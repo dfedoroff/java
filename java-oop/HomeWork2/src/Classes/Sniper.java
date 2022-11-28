61637e5647d0565379c68ed80e1eae873a06d48f
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-11-29 00:18:05 +0700

    refactor: Implement interface Stepable in Sniper class

--- java-oop/HomeWork2/src/Classes/Sniper.java
+++ java-oop/HomeWork2/src/Classes/Sniper.java
@@ -1,6 +1,17 @@
-public class Sniper extends BasicCharacter {
+package Classes;
 
-    public Sniper() {
-        super(12, 10, 32, new int[]{8,10}, 15, 9, false, false);
+import Interfaces.Stepable;
+
+import java.util.ArrayList;
+
+public class Sniper extends BasicCharacter implements Stepable {
+
+    public Sniper(ArrayList<BasicCharacter> squad) {
+        super(4, 5, 0, new int[]{1,3}, 10, 4, false, false);
+        super.squad = squad;
+    }
+
+    @Override
+    public void step() {
     }
 }
