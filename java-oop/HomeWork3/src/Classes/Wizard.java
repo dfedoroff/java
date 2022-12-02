be34f2012cad728a8b364294bb3ba0db2d650c6d
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-02 08:42:54 +0700

    Add Wizard.java

---
+++ java-oop/HomeWork3/src/Classes/Wizard.java
@@ -0,0 +1,29 @@
+package Classes;
+
+import Interfaces.Stepable;
+
+import java.util.ArrayList;
+
+public class Wizard extends BasicCharacter implements Stepable {
+
+    public Wizard(ArrayList<BasicCharacter> squad) {
+        super(17, 12, 0, new int[]{-5, -5}, 30, 9, false, true);
+        super.squad = squad;
+    }
+
+    @Override
+    public void step() {
+        BasicCharacter characterDamaged = squad.get(0);
+        for (BasicCharacter character : squad) {
+            if (character.getHealth() / character.getMaxHealth() < characterDamaged.getHealth() / characterDamaged.getMaxHealth()) {
+                characterDamaged = character;
+            }
+        }
+
+        characterDamaged.setHealth(characterDamaged.getHealth() - getDamage()[1]);
+
+        if (characterDamaged.getHealth() > characterDamaged.getMaxHealth()) {
+            characterDamaged.setHealth(characterDamaged.getMaxHealth());
+        }
+    }
+}
