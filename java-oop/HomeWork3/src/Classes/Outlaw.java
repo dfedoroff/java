ed3f0e16f29893dd36bfb0acc02bb6d54baa8774
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-05 00:21:04 +0700

    refactor: Change code in Outlaw class

--- java-oop/HomeWork3/src/Classes/Outlaw.java
+++ java-oop/HomeWork3/src/Classes/Outlaw.java
@@ -1,17 +1,9 @@
 package Classes;
 
-import Interfaces.Stepable;
+public class Outlaw extends BasicCharacter {
 
-import java.util.ArrayList;
-
-public class Outlaw extends BasicCharacter implements Stepable {
-
-    public Outlaw(ArrayList<BasicCharacter> squad) {
-        super(8, 3, 0, new int[]{2,4}, 10, 6, false, false);
-        super.squad = squad;
-    }
-
-    @Override
-    public void step() {
+    public Outlaw(String squad, int x, int y) {
+        super("Outlaw", 8, 3, 0, new int []{2, 4}, 10, 6, false, false, squad);
+        super.position = new Coordinates(x, y);
     }
 }
