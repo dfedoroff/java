81b6b85a778784e666564072fec2948206e722b5
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-11 18:26:55 +0700

    feat: Assign random integer value for each element of array of predifined size

--- java-exceptions/HomeWork1/src/Task3.java
+++ java-exceptions/HomeWork1/src/Task3.java
@@ -1,8 +1,21 @@
+import java.util.Random;
+
 public class Task3 {
 
 	public static void main(String[] args) {
 
 		int[] firstArray = fillArray(10);
 		int[] secondArray = fillArray(10);
+	}
+
+	public static int[] fillArray(int len) {
+
+		int[] array = new int[len];
+		Random rand = new Random();
+
+		for (int i = 0; i < len; i++) {
+			array[i] = rand.nextInt(1, 10);
+		}
+		return array;
 	}
 }
