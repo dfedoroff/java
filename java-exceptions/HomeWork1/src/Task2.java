01dc16984407facb8ed1d72c69dec3ae27f1f8a8
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-12 20:26:06 +0700

    style: Replace tab indents with four spaces

--- java-exceptions/HomeWork1/src/Task2.java
+++ java-exceptions/HomeWork1/src/Task2.java
@@ -2,38 +2,37 @@ import java.util.regex.Pattern;
 
 public class Task2 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        String[][] strArr = { { "3", "7", "5", "-6", "you", "3" }, { "9", null, "2", "8", "-", "5" } };
+        System.out.println(sum2d(strArr));
+    }
 
-		String[][] strArr = { { "3", "7", "5", "-6", "you", "3" }, { "9", null, "2", "8", "-", "5" } };
-		System.out.println(sum2d(strArr));
-	}
+    public static int sum2d(String[][] arr) {
 
-	public static int sum2d(String[][] arr) {
+        int sum = 0;
 
-		int sum = 0;
+        if (arr != null) {
+            for (int i = 0; i < arr.length; i++) {  //NullPointerException
+                for (int j = 0; j < arr[i].length; j++) {  //IndexOutOfBoundsException
+                    if (arr[i][j] != null) {
+                        if (isNumber(arr[i][j])) {
+                            int val = Integer.parseInt(arr[i][j]);  //NumberFormatException
+                            sum += val;
+                        }
+                    }
+                }
+            }
+        }
+        return sum;
+    }
 
-		if (arr != null) {
-			for (int i = 0; i < arr.length; i++) {  //NullPointerException
-				for (int j = 0; j < arr[i].length; j++) {  //IndexOutOfBoundsException
-					if (arr[i][j] != null) {
-						if (isNumber(arr[i][j])) {
-							int val = Integer.parseInt(arr[i][j]);  //NumberFormatException
-							sum += val;
-						}
-					}
-				}
-			}
-		}
-		return sum;
-	}
+    public static boolean isNumber(String array) {
 
-	public static boolean isNumber(String array) {
+        Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
-		Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
-
-		if (array == null) {
-			return false;
-		}
-		return pattern.matcher(array).matches();
-	}
+        if (array == null) {
+            return false;
+        }
+        return pattern.matcher(array).matches();
+    }
 }
