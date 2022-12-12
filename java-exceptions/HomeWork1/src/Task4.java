59f25f2528eb3deb464ff388efd846a3ddee6c7e
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-12 20:29:46 +0700

    style: Replace tab indents with four spaces

--- java-exceptions/HomeWork1/src/Task4.java
+++ java-exceptions/HomeWork1/src/Task4.java
@@ -3,45 +3,45 @@ import java.util.Random;
 
 public class Task4 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[] firstArray = fillArray(10);
-		System.out.println("Первый массив:");
-		printArray(firstArray);
-		int[] secondArray = fillArray(9);
-		System.out.println("Второй массив:");
-		printArray(secondArray);
-		System.out.println("Массив частных от целочисленного деления элементов двух входящих массивов:");
-		printArray(divideArrays(firstArray, secondArray));
+        int[] firstArray = fillArray(10);
+        System.out.println("Первый массив:");
+        printArray(firstArray);
+        int[] secondArray = fillArray(9);
+        System.out.println("Второй массив:");
+        printArray(secondArray);
+        System.out.println("Массив частных от целочисленного деления элементов двух входящих массивов:");
+        printArray(divideArrays(firstArray, secondArray));
 
-	}
+    }
 
-	public static int[] divideArrays(int[] firstArr, int[] secondArr) {
+    public static int[] divideArrays(int[] firstArr, int[] secondArr) {
 
-		if (firstArr.length != secondArr.length) {
-			throw new RuntimeException("Массивы разной длины.");
-		}
+        if (firstArr.length != secondArr.length) {
+            throw new RuntimeException("Массивы разной длины.");
+        }
 
-		int[] array = new int[firstArr.length];
+        int[] array = new int[firstArr.length];
 
-		for (int i = 0; i < array.length; i++) {
-			array[i] = firstArr[i] / secondArr[i];
-		}
-		return array;
-	}
+        for (int i = 0; i < array.length; i++) {
+            array[i] = firstArr[i] / secondArr[i];
+        }
+        return array;
+    }
 
-	public static int[] fillArray(int len) {
+    public static int[] fillArray(int len) {
 
-		int[] arr = new int[len];
-		Random rand = new Random();
+        int[] arr = new int[len];
+        Random rand = new Random();
 
-		for (int i = 0; i < len; i++) {
-			arr[i] = rand.nextInt(1, 10);
-		}
-		return arr;
-	}
+        for (int i = 0; i < len; i++) {
+            arr[i] = rand.nextInt(1, 10);
+        }
+        return arr;
+    }
 
-	public static void printArray(int[] arr) {
-		System.out.println(Arrays.toString(arr));
-	}
+    public static void printArray(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
 }
