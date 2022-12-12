1eeabb4b1517440e57439a317ce04b4bf1ef7730
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-12 07:27:36 +0700

    feat: Print contents of array of integers

--- java-exceptions/HomeWork1/src/Task4.java
+++ java-exceptions/HomeWork1/src/Task4.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task4 {
@@ -17,5 +18,9 @@ public class Task4 {
 			arr[i] = rand.nextInt(1, 10);
 		}
 		return arr;
+	}
+
+	public static void printArray(int[] arr) {
+		System.out.println(Arrays.toString(arr));
 	}
 }
