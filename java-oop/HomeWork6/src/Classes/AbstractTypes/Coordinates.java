ac5acd0d06c70651e0586fc6af4783be6b50f715
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-13 09:58:50 +0700

    Add Cordinates.java

---
+++ java-oop/HomeWork6/src/Classes/AbstractTypes/Coordinates.java
@@ -0,0 +1,25 @@
+package Classes.AbstractTypes;
+
+import static java.lang.Math.abs;
+
+public class Coordinates {
+    public int x;
+    public int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double getDistance(Coordinates a, Coordinates b) {
+        return Math.hypot(abs(b.x - a.x), abs(b.y - a.y));
+    }
+
+    public boolean isClose(Coordinates b){
+        return abs(x - b.x) <= 1 && abs(y - b.y) <= 1;
+    }
+
+    public boolean isEqual(Coordinates position) {
+        return position.y == y && position.x == x;
+    }
+}
