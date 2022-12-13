de8129b7f09f7146799e2bd49564334ca4560f9e
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-13 09:57:02 +0700

    Add Wizard.java

---
+++ java-oop/HomeWork6/src/Classes/Wizard.java
@@ -0,0 +1,13 @@
+package Classes;
+
+import Classes.AbstractTypes.Coordinates;
+import Classes.AbstractTypes.Healers;
+import Classes.AbstractTypes.Squad;
+
+public class Wizard extends Healers {
+
+    public Wizard(int amount, int line, int column, Squad allies, Squad enemies) {
+
+        super(new Coordinates(line, column), 'W', 17, 12, new int[]{-2, -2}, 30, 9, allies, enemies, amount);
+    }
+}
