9d45f6b588a5315dfe74e76604aa83568811d0f1
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-15 17:28:00 +0700

    refactor: Change order of exceptions to catch most specific first and then most generic exceptions

--- java-exceptions/HomeWork2/src/Task3.java
+++ java-exceptions/HomeWork2/src/Task3.java
@@ -6,6 +6,17 @@ public class Task3 {
         abc[3] = 9;
         int a = 90;
         int b = 3;
+
+        try {
+            System.out.println(a / b);
+            printSum(23, 234);
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
     }
 
     public static void printSum(Integer a, Integer b) {
