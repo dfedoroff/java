ee09c99d7f8c835b7ec9e43aeee3909489518a8f
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-16 12:55:59 +0700

    Add Outlaw.java

---
+++ java-oop/HomeWork7/src/Classes/Outlaw.java
@@ -0,0 +1,13 @@
+package Classes;
+
+import Classes.AbstractTypes.Coordinates;
+import Classes.AbstractTypes.Squad;
+import Classes.AbstractTypes.Warriors;
+
+public class Outlaw extends Warriors {
+
+    public Outlaw(int amount, int line, int column, Squad allies, Squad enemies) {
+
+        super(new Coordinates(line, column), 'O', 8, 3, new int[]{2, 4}, 10, 6, allies, enemies, amount);
+    }
+}
