d95a25fc120ac2279a883471ebcc2561b77a2266
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-12-16 12:56:18 +0700

    Add Sniper.java

---
+++ java-oop/HomeWork7/src/Classes/Sniper.java
@@ -0,0 +1,12 @@
+package Classes;
+
+import Classes.AbstractTypes.Coordinates;
+import Classes.AbstractTypes.Shooters;
+import Classes.AbstractTypes.Squad;
+
+public class Sniper extends Shooters {
+    public Sniper(int amount, int line, int column, Squad allies, Squad enemies) {
+        super(new Coordinates(line, column), 'S', 4, 5, new int[]{1, 3}, 10, 4, allies, enemies, amount);
+        this.shots = 16;
+    }
+}
