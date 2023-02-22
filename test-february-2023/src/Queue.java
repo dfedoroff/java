45432d7900fcb25394b520a9ae4ef1afe5edc1d6
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-02-22 15:27:02 +0700

    feat: Create instance of class and fills it with toys from database

---
+++ test-february-2023/src/Queue.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+
+public class Queue {
+    PriorityQueue<Toy> queue = new PriorityQueue<>();
+    boolean removeAfterGetting = false;
+
+    public Queue(ToyManager manager, int quantity) {
+        Toy toy = manager.nextToy(false);
+
+        for (int i = 0; i < quantity && toy != null; i++) {
+            queue.offer(toy);
+            toy = manager.nextToy(false);
+        }
+    }
+}
