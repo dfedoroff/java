734634c80bff0db607f6d7e9133ed82499e29d6c
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-02-22 13:36:26 +0700

    Add bash script files for linux task 1.5.

---
+++ test-february-2023/bash_scripts/add_cars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file_path="/home/ubuntu/Documents/geek_brains/java/test-february-2023/Игрушки/игрушки_для_дошколят/машинки"
+id=$(<carsfile)
+id=$((id + 1))
+toy="машинка"
+weight=$(shuf -i 0-100 -n 1)
+FLOOR=0
+RANGE=100
+while [ "$weight" -le $FLOOR ]; do
+  weight=$RANDOM
+  let "weight %= $RANGE"
+done
+text_line="Id: ${id} Toy: ${toy} Weight: ${weight}"
+echo "${text_line}" >> "${file_path}"
+echo "${id}" > carsfile
+
---
+++ test-february-2023/bash_scripts/add_softtoys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file_path="/home/ubuntu/Documents/geek_brains/java/test-february-2023/Игрушки/игрушки_для_дошколят/мягкие_игрушки"
+id=$(<softtoysfile)
+id=$((id + 1))
+toy="мягкая_игрушка"
+weight=$(shuf -i 0-100 -n 1)
+FLOOR=0
+RANGE=100
+while [ "$weight" -le $FLOOR ]; do
+  weight=$RANDOM
+  let "weight %= $RANGE"
+done
+text_line="Id: ${id} Toy: ${toy} Weight: ${weight}"
+echo "${text_line}" >> "${file_path}"
+echo "${id}" > softtoysfile
+
