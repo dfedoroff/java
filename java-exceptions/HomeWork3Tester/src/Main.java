7e3c5bf8ab7fc39653c466f79b44080e3f6bb000
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-02-26 23:42:36 +0700

    dc: Print message about successful or unsuccessful registration depending on value of boolean variable

--- java-exceptions/HomeWork3Tester/src/Main.java
+++ java-exceptions/HomeWork3Tester/src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         String confirmPassword = "19_LatinLettersNums";
 
         boolean isRegistered = Registration.register(login, password, confirmPassword);
+
+        if (isRegistered) {
+            System.out.println("Регистрация завершена успешно");
+        } else {
+            System.out.println("Регистрация прошла неудачно!");
+        }
     }
 }
