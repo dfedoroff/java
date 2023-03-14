87d8ca77a823f7883695fbeb26bf67f63253880e
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-03-14 08:34:50 +0700

    feat: Sort elements of array using heapsort algorithm

--- algorithms-and-data-structures/HomeWork1/src/HeapSort.java
+++ algorithms-and-data-structures/HomeWork1/src/HeapSort.java
@@ -1,5 +1,18 @@
 public class HeapSort {
 
+    public static void sort(int[] arr) {
+        int len = arr.length;
+
+        for (int i = len / 2 - 1; i >= 0; i--) {
+            buildHeap(arr, len, i);
+        }
+
+        for (int i = len - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            buildHeap(arr, i, 0);
+        }
+    }
+
     public static void buildHeap(int[] arr, int heapLen, int i) {
         int left = 2 * i + 1;
         int right = 2 * i + 2;
