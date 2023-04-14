e722b80e2894c8c4006bc257897ad2d43d37a5bc
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-04-14 14:45:27 +0700

    feat: Define Skill class with record constructor accepting name and description additional constructor that takes only name and custom implementations for equals, hashCode and toString methods, focusing on name field for comparison and representation

---
+++ final-certification-2023/src/com/kennel/model/Skill.java
@@ -0,0 +1,27 @@
+package com.kennel.model;
+
+import java.util.Objects;
+
+public record Skill(String name, String description) {
+    public Skill(String name) {
+        this(name, null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Skill skill = (Skill) o;
+        return name.equals(skill.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
