9b7964158c8d98eea5cde49e1945e62691b7dea6
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-04-14 14:35:02 +0700

    feat: Define Dog class that extends AbstractPet class takes name and birth date as parameters and sets its AnimalGenius property to DOG

---
+++ final-certification-2023/src/com/kennel/model/implement/Dog.java
@@ -0,0 +1,13 @@
+package com.kennel.model.implement;
+
+import com.kennel.model.AbstractPet;
+import com.kennel.model.AnimalGenius;
+
+import java.time.LocalDate;
+
+public class Dog extends AbstractPet {
+    public Dog(String name, LocalDate birthDate) {
+        super(name, birthDate);
+        setAnimalGenius(AnimalGenius.DOG);
+    }
+}
