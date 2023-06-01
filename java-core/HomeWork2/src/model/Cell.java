43f0c16c48330f1a2530adee551227b1cfb9e38d
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-06-01 23:33:33 +0700

    feat: Create Cell model

---
+++ java-core/HomeWork2/src/model/Cell.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Cell {
+    private char value;
+
+    public Cell(char value) {
+        this.value = value;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+}
+
