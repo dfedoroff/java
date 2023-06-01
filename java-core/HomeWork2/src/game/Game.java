e90ac054f862a4332d55a56922ab672fa67c63a3
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-06-01 23:34:12 +0700

    feat: Implement Game class

---
+++ java-core/HomeWork2/src/game/Game.java
@@ -0,0 +1,62 @@
+package game;
+
+import model.GameField;
+import player.Player;
+import player.HumanPlayer;
+import player.AIPlayer;
+
+import java.util.Scanner;
+
+public class Game {
+    private GameField field;
+    private Player humanPlayer;
+    private Player aiPlayer;
+    private int currentTurn = 1;
+
+    public Game() {
+        this.field = new GameField();
+        this.humanPlayer = new HumanPlayer();
+        this.aiPlayer = new AIPlayer();
+    }
+
+    public void start() {
+        while (true) {
+            playRound();
+            System.out.print("Сыграть еще раз? ");
+            if (!"Y".equalsIgnoreCase(new Scanner(System.in).next())) {
+                break;
+            }
+        }
+    }
+
+    private void playRound() {
+        field.initField();
+        field.printField();
+        while (true) {
+            humanPlayer.makeMove(field);
+            field.printField();
+            if (checkEndGame('X')) break;
+
+            aiPlayer.makeMove(field);
+            field.printField();
+            if (checkEndGame('O')) break;
+        }
+    }
+
+    public boolean checkEndGame(char dot) {
+        if (field.checkWin(dot)) {
+            if (dot == 'X') {
+                System.out.println("Человек победил!");
+            } else {
+                System.out.println("ИИ побеждает!");
+            }
+            return true;
+        }
+        if (currentTurn > field.getRows() * field.getColumns()) {
+            System.out.println("Ничья!");
+            return true;
+        }
+        return false;
+    }
+}
+
